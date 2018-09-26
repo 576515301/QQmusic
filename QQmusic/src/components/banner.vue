4116fe6e735201ea8066c25c85bf80ad06d53c8d
NlMABl <576515301@qq.com>
2018-09-26 23:22:31 +0800

    1
    1

--- QQmusic/src/components/banner.vue
+++ QQmusic/src/components/banner.vue
@@ -1,16 +1,16 @@
 <template>
 	<div id="banner">
-		<ul class="bannerHk">
+		<ul class="bannerHk" ref="bannerHk">
 			<li 
 				class="bannerItem"
+				:class="{befor:beforBanner==index,now:nowBanner==index,after:afterBanner==index}"
 				v-for="(v,index) in banner"
+				:style="{left:0,zIndex:index}"
 			>
-			{{bannerBox[0].id}}
 				<img :src="v.picUrl">
-			}
-			}
 			</li>
 		</ul>
+		<img v-cloak class="bgImg" :src="bannerBox[0].picUrl">
 	</div>
 </template>
 
@@ -19,18 +19,47 @@
 		props:["banner"],
 		data(){
 			return {
-				bannerBox:this.banner
+				bannerBox:[{}],
+				bannerClass:{},
+				beforBanner:null,
+				nowBanner:null,
+				afterBanner:null,
+				timer:null,
 			}
 		},
-		created(){
-			
+		methods:{
+			bannerMove(){
+				this.timer = setInterval(()=>{
+					if(this.nowBanner == this.bannerBox.length - 1){
+						this.nowBanner = 0
+					}else{
+						this.nowBanner++
+					}
+				},2000);
+			}
 		},
 		watch:{
-			bannerBox:{
+			banner:{
 				handler(n,o){
-					console.log(n);
+					this.bannerBox = n;
+					this.nowBanner = 0;
+					this.bannerMove();
 				},
 				deep:true
+			},
+			nowBanner(n,o){
+				var item = document.querySelectorAll(".bannerItem");
+				console.log(item);
+				if(n == 0){
+					this.beforBanner = this.bannerBox.length - 1;
+					this.afterBanner = n + 1;
+				}else if(n == this.bannerBox.length - 1){
+					this.beforBanner = n - 1;
+					this.afterBanner = 0;
+				}else{
+					this.beforBanner = n - 1;
+					this.afterBanner = n + 1;
+				}
 			}
 		}
 	}
@@ -38,13 +67,35 @@
 
 <style lang="less" scoped>
 	#banner{
+		overflow:hidden;
+		&>img.bgImg{
+			width:100%;
+			visibility: hidden;
+		}
 		.bannerHk{
+			position:relative;
 			width:100%;
 			.bannerItem{
+				top:0;
+				position:absolute;
 				width:100%;
-				img{
+				transition-duration: 400ms;
+				display: none;
+				&.befor{
 					display: block;
+					transform: translate(-100%,0);
+				}
+				&.now{
+					display: block;
+					transform: translate(0,0);
+				}
+				&.after{
+					display: block;
+					transform: translate(100%,0);
+				}
+				img{
 					width:100%;
+					display: block;
 				}
 			}
 		}
